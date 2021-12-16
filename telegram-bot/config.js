72a3629b4c9cd2ed8977554145aad2a610dedd6f
Yaroslav <yaroslavnekryach@gmail.com>
2021-12-16 17:49:05 +0200

    Base bot implementation

---
+++ telegram-bot/config.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const nearApi = require('near-api-js');
+
+const NETWORK =  process.env.NETWORK;
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const BOT_NAME = process.env.BOT_NAME;
+const CONTRACT = process.env.CONTRACT;
+const RPC = `https://rpc.${NETWORK}.near.org`;
+const CALLBACK_URL = `https://t.me/${BOT_NAME}`;
+const PROVIDER = new nearApi.providers.JsonRpcProvider({url: RPC})
+
+
+module.exports = {
+    NETWORK,
+    BOT_TOKEN,
+    BOT_NAME,
+    CONTRACT,
+    RPC,
+    CALLBACK_URL,
+    PROVIDER
+}
